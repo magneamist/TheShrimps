fab04e6bae08e8210b1d148586d948b3140e2ba4
Daniel Yezid Roncancio Garcia <166632468+DaniYezid@users.noreply.github.com>
2025-04-03 09:33:00 +0200

    added services

--- backend/models/userDetailModel.js
+++ backend/models/userDetailModel.js
@@ -1,14 +1,20 @@
 import sequelize from '../configs/dbConfig.js';
 import { DataTypes, Model } from 'sequelize';
 
-export class userDetailModel extends Model {}
+export class UserDetailModel extends Model { }
 
-userDetailModel.init({
+UserDetailModel.init({
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
     allowNull: false
+  },
+
+  clerk_user_id: {  // Identificador de Clerk
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
   },
 
   firstname: {
@@ -31,11 +37,6 @@ userDetailModel.init({
     type: DataTypes.STRING
   },
 
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
   city: {
     type: DataTypes.STRING
   },
@@ -52,37 +53,12 @@ userDetailModel.init({
     type: DataTypes.ENUM("YES", "NO")
   },
 
-  pfp: {
+  profile_image: {  // Nombre consistente con la tabla
     type: DataTypes.STRING
   },
 
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  }
+  // created_at: {
+  //   type: DataTypes.DATE,
+  //   defaultValue: DataTypes.NOW
+  // }
 })
-
-// backend/models/user.js
-// const { DataTypes } = require("sequelize");
-// const sequelize = require("../config/database");
-
-// const User = sequelize.define("usersDetails", {
-//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-//   first_Name: { type: DataTypes.STRING, allowNull: false },
-//   last_Name: { type: DataTypes.STRING, allowNull: false },
-//   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-//   password: { type: DataTypes.STRING, allowNull: false },
-//   phone_Num: { type: DataTypes.STRING },
-//   street: { type: DataTypes.STRING },
-//   city: { type: DataTypes.STRING },
-//   country: { type: DataTypes.STRING },
-//   zip_Code: { type: DataTypes.INTEGER },
-//   bill_Address: { type: DataTypes.STRING },
-//   favorite: { type: DataTypes.ENUM("yes", "no") },
-//   profile_image: { type: DataTypes.STRING },  // Ruta de la imagen de perfil
-//   created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-// }, {
-//   timestamps: false, // No crea `createdAt` y `updatedAt` automáticamente
-// });
-
-// module.exports = User;
