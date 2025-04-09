b08c8e632432281d6c8904e5af4ba893e1f8d610
Daniel Yezid Roncancio Garcia <166632468+DaniYezid@users.noreply.github.com>
2025-04-09 13:02:33 +0100

    Dockerfile

--- backend/configs/dbConfig.js
+++ backend/configs/dbConfig.js
@@ -4,19 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = new Sequelize(
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASS,
-    "shrimpsclothes",
-    "root",
-    "1234",
-    // "Alumno2425",
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false,
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  }
 );
 
 export default dbConfig;
