e6cc4d75e2a4afa4e7ef7e7b7cd3f757ee1fb41a
magneamist <magneamist@gmail.com>
2025-04-09 09:39:21 +0100

    added function to the back button

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Header({ title }: { title?: string }) {
+  const router = useRouter();
+
   return (
     <div className="relative w-full">
       <div className="flex w-full justify-center pb-4">
-        <ArrowLeft
-          strokeWidth={1.5}
-          size={34}
-          className="absolute top-0 left-0"
-        />
+        <button onClick={() => router.back()}>
+          <ArrowLeft
+            strokeWidth={1.5}
+            size={34}
+            className="absolute top-0 left-0"
+          />
+        </button>
         <h1 className="text-2xl">{title}</h1>
       </div>
     </div>
